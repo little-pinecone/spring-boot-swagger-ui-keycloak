a5237759526a6771011e61c2a321add467758351
little-pinecone <marta.szymek.mail@gmail.com>
2021-10-19 19:14:40 +0200

    Initial commit

---
+++ src/main/java/in/keepgrowing/springbootswaggeruikeycloak/SpringBootSwaggerUiKeycloakApplication.java
@@ -0,0 +1,13 @@
+package in.keepgrowing.springbootswaggeruikeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSwaggerUiKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSwaggerUiKeycloakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/keepgrowing/springbootswaggeruikeycloak/SpringBootSwaggerUiKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package in.keepgrowing.springbootswaggeruikeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSwaggerUiKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
