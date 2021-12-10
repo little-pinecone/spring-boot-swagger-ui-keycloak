89354088345f24f1742ea946ab4510cc45501832
little-pinecone <marta.szymek.mail@gmail.com>
2021-12-10 11:28:48 +0100

    feat: update Spring Boot to 2.6.1 * use new Spring Security annotation to enable method security * update test config

--- src/main/java/in/keepgrowing/springbootswaggeruikeycloak/shared/infrastructure/config/security/CustomMethodSecurityConfig.java
+++ src/main/java/in/keepgrowing/springbootswaggeruikeycloak/shared/infrastructure/config/security/CustomMethodSecurityConfig.java
@@ -1,10 +1,7 @@
 package in.keepgrowing.springbootswaggeruikeycloak.shared.infrastructure.config.security;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
-@Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class CustomMethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+@EnableMethodSecurity
+public class CustomMethodSecurityConfig {
 }
--- src/test/java/in/keepgrowing/springbootswaggeruikeycloak/shared/infrastructure/config/ControllerIntegrationTestConfig.java
+++ src/test/java/in/keepgrowing/springbootswaggeruikeycloak/shared/infrastructure/config/ControllerIntegrationTestConfig.java
@@ -1,9 +1,9 @@
 package in.keepgrowing.springbootswaggeruikeycloak.shared.infrastructure.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @TestConfiguration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class ControllerIntegrationTestConfig {
 }
